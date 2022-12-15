703ce30dc306c53ac158aa924b67c073d2a355bb
paulmungala <mungalaaa8@gmail.com>
2022-12-15 15:23:40 +0300

    my_lint_works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
-      <Head>
-        <title>Github User Finder</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
-}
+    return (
+        <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
+            <Head>
+                <title>Github User Finder</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
